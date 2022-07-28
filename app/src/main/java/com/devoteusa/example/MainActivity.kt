71bb9ded347b66a959bf108de15b2d165bfd983b
NoamDev <noam.yem@gmail.com>
2022-07-28 14:01:34 +0300

    remove usage from MainActivity

--- app/src/main/java/com/devoteusa/example/MainActivity.kt
+++ app/src/main/java/com/devoteusa/example/MainActivity.kt
@@ -3,7 +3,6 @@ package com.devoteusa.example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.devoteusa.example.databinding.ActivityMainBinding
-import com.devoteusa.devote.DeVoteJNI
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +13,5 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Example of a call to a native method
-        var pk = ByteArray(32);
-        var sk = ByteArray(32);
-
-        DeVoteJNI.generateVoterKeypair(pk, sk);
     }
 }
